5c9eb1e5e4d1b576c7c5ca04e8dac9b792f9864e
muazmd <80607495+md1999-m@users.noreply.github.com>
2021-05-26 21:12:01 +0300

    icon

--- MAPS_strategy.php
+++ MAPS_strategy.php
@@ -65,7 +65,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" class="btn btn-outline-info" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- 
+    <script src="https://kit.fontawesome.com/f5f92436ce.js" crossorigin="anonymous"> </script>
  <style>  
     
         body{
