992a489a0ace2f1047cf6f2b19efd8df83736895
muazmd <80607495+md1999-m@users.noreply.github.com>
2021-05-26 20:32:49 +0300

    updated register

--- registerForm.php
+++ registerForm.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
 		$user = md5($username);
 		$password=$_POST['password'];
 		$pass = md5($password)		
-		$con= mysqli_connect("localhost", "root","", "mapsstore");
+		$con= mysqli_connect("den1.mysql6.gear.host", "mymapstore","Om78!Bi!M209", "mymapstore");
 		$sql="INSERT INTO  user (username,password, role) VALUES ('$user','$pass',2)";
         $result=mysqli_query($con,$sql);
         header("location: index2.php");}}
